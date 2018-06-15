7d9095d847a5eff70b448701705c5b975f061e36
Erik van Sebille <erikvansebille@gmail.com>
2018-06-15 10:08:07 +0200

    Adding Stokes drift to simulations

--- plastic_antarcticpeninsula_run.py
+++ plastic_antarcticpeninsula_run.py
@@ -21,11 +21,43 @@ def set_hycom_grid(files):
     return fset
 
 
+def set_ww3_grid(stokesfiles, fset):
+
+    dimensions = {'lat': 'latitude', 'lon': 'longitude', 'time': 'time'}
+    uuss = Field.from_netcdf(stokesfiles, 'uuss', dimensions)
+    vuss = Field.from_netcdf(stokesfiles, 'vuss', dimensions)
+    uuss.add_periodic_halo(zonal=True, meridional=False, halosize=3)
+    vuss.add_periodic_halo(zonal=True, meridional=False, halosize=3)
+    uuss.units = fset.U.units
+    vuss.units = fset.V.units
+    return uuss, vuss
+
+
 def WrapLon(particle, fieldset, time, dt):
     if particle.lon > fieldset.halo_east:
         particle.lon = particle.lon - 360.
     if particle.lon < fieldset.halo_west:
         particle.lon = particle.lon + 360.
+
+
+def AdvectionRK4_stokes(particle, fieldset, time, dt):
+    u1 = fieldset.U[time, particle.lon, particle.lat, particle.depth] + fieldset.uuss[time, particle.lon, particle.lat, particle.depth]
+    v1 = fieldset.V[time, particle.lon, particle.lat, particle.depth] + fieldset.vuss[time, particle.lon, particle.lat, particle.depth]
+
+    lon1, lat1 = (particle.lon + u1 * .5 * dt, particle.lat + v1 * .5 * dt)
+    u2 = fieldset.U[time + .5 * dt, lon1, lat1, particle.depth] + fieldset.uuss[time + .5 * dt, lon1, lat1, particle.depth]
+    v2 = fieldset.V[time + .5 * dt, lon1, lat1, particle.depth] + fieldset.vuss[time + .5 * dt, lon1, lat1, particle.depth]
+
+    lon2, lat2 = (particle.lon + u2 * .5 * dt, particle.lat + v2 * .5 * dt)
+    u3 = fieldset.U[time + .5 * dt, lon2, lat2, particle.depth] + fieldset.uuss[time + .5 * dt, lon2, lat2, particle.depth]
+    v3 = fieldset.V[time + .5 * dt, lon2, lat2, particle.depth] + fieldset.vuss[time + .5 * dt, lon2, lat2, particle.depth]
+
+    lon3, lat3 = (particle.lon + u3 * dt, particle.lat + v3 * dt)
+    u4 = fieldset.U[time + dt, lon3, lat3, particle.depth] + fieldset.uuss[time + dt, lon3, lat3, particle.depth]
+    v4 = fieldset.V[time + dt, lon3, lat3, particle.depth] + fieldset.vuss[time + dt, lon3, lat3, particle.depth]
+
+    particle.lon += (u1 + 2 * u2 + 2 * u3 + u4) / 6. * dt
+    particle.lat += (v1 + 2 * v2 + 2 * v3 + v4) / 6. * dt
 
 
 def BoundaryVels(particle, fieldset, time, dt):
@@ -39,9 +71,13 @@ def OutOfBounds(particle, fieldset, time, dt):
     particle.delete()
 
 
-def run_hycom_particles(lons, lats, locs, rundays, files):
+def run_hycom_particles(lons, lats, locs, rundays, files, stokesfiles):
     print(locs)
     fset = set_hycom_grid(files)
+    uuss, vuss = set_ww3_grid(stokesfiles, fset)
+    fset.add_field(uuss)
+    fset.add_field(vuss)
+
     size2D = (fset.U.grid.ydim, fset.U.grid.xdim)
     fset.add_field(Field('Kh_zonal', data=10*np.ones(size2D), lon=fset.U.grid.lon, lat=fset.U.grid.lat, mesh='spherical', allow_time_extrapolation=True))
     fset.add_field(Field('Kh_meridional', data=10*np.ones(size2D), lon=fset.U.grid.lon, lat=fset.U.grid.lat, mesh='spherical', allow_time_extrapolation=True))
@@ -50,7 +86,7 @@ def run_hycom_particles(lons, lats, locs, rundays, files):
     pset = ParticleSet.from_list(fieldset=fset, pclass=JITParticle, lon=np.tile(lons, [nperloc]),
                                  lat=np.tile(lats, [nperloc]))
 
-    ofile = pset.ParticleFile(name='antarcticplastic_5yr_%02d.nc' % locs[0], outputdt=delta(days=1))
+    ofile = pset.ParticleFile(name='antarcticplastic_wstokes_5yr_%02d.nc' % locs[0], outputdt=delta(days=1))
 
     kernels = pset.Kernel(AdvectionRK4) + BrownianMotion2D + BoundaryVels + WrapLon
     pset.execute(kernels, runtime=delta(days=rundays), dt=-delta(minutes=5),
@@ -73,12 +109,14 @@ if __name__ == "__main__":
     else:
         to_run = range(0, 12)
 
-    # ddir = '/Volumes/data01/HYCOMdata/GLBa0.08_expt90_surf/hycom_GLBu0.08_912_'
-    ddir = '/Users/erik/Desktop/HycomAntarctic/hycom_GLBu0.08_912_'
+    ddir = '/Volumes/data01/HYCOMdata/GLBa0.08_expt90_surf/hycom_GLBu0.08_912_'
+    # ddir = '/Users/erik/Desktop/HycomAntarctic/hycom_GLBu0.08_912_'
+
+    stokesfiles=sorted(glob('/Volumes/data01/WaveWatch3data/WW3*'))
     allfiles = sorted(glob(ddir + "201*00_t000.nc"))
     rundays = 365 * 5
 
     for i in to_run:
         ifiles = [j for j, s in enumerate(allfiles) if "201702%02d" % days[i] in s]
         files = allfiles[ifiles[0] - rundays - 5:ifiles[0]]
-        run_hycom_particles([lons[i]], [lats[i]], [locs[i]], rundays, files)
+        run_hycom_particles([lons[i]], [lats[i]], [locs[i]], rundays, files, stokesfiles)
